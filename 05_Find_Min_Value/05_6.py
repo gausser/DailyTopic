c02302bef0b51122469081f8556da8ec24d6f818
Watermelon <wan198809@163.com>
2017-09-07 22:42:35 +0800

    05_Find_Min_Value

---
+++ 05_Find_Min_Value/05_6.py
@@ -0,0 +1,6 @@
+
+first = int(raw_input("Enter the first value: "))
+second = int(raw_input("Enter the second value: "))
+third = int(raw_input("Enter the third value: "))
+
+print min(first, second, third)
